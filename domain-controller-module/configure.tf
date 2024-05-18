28630a849dcbd51a1413b862957cde0d8ddfd94a
the3venthoriz0n <indrewjones@gmail.com>
2024-05-17 19:05:51 -0700

    Fix configuration and powershell vs az issues

--- domain-controller-module/configure.tf
+++ domain-controller-module/configure.tf
@@ -1,33 +1,16 @@
-
-# resource "null_resource" "run_az_cli" {
-#   count = var.configure ? 2 : 0 # If configure is set to true, run 
-
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       powershell -File "${path.module}/azcli_configure.ps1" `
-#         -ADAdminPassword "${var.ad_admin_password}" `
-#         -Location "${var.location}" `
-#         -ResourceGroupName "${var.resource_group_name}" `
-#         -DomainController1 "${azurerm_windows_virtual_machine.dc[0].name}" `
-#         -DomainController2 "${azurerm_windows_virtual_machine.dc[1].name}" `
-#         -DomainControllerAll "${azurerm_windows_virtual_machine.dc[count.index].name}" `
-#         -ADForestName "${var.domain_name}"
-#     EOT
-#   }
-
-#   # Ensure the VMs are created before running the command
-#   depends_on = [
-#     azurerm_windows_virtual_machine.dc
-#   ]
-# }
-
-# TODO edit path and use variable version
-resource "null_resource" "run_az_cli_hardcode" {
+resource "null_resource" "run_az_cli" {
   count = var.configure ? 2 : 0 # If configure is set to true, run 
 
   provisioner "local-exec" {
     command = <<EOT
-      powershell -File "${path.module}/azcli_configure_hardcode.ps1"
+      powershell -File "X:\domain-controller-module\azcli_configure_hardcode.ps1" `
+        -ADAdminPassword "${var.ad_admin_password}" `
+        -Location "${var.location}" `
+        -ResourceGroupName "${var.resource_group_name}" `
+        -DomainController1 "${azurerm_windows_virtual_machine.dc[0].name}" `
+        -DomainController2 "${azurerm_windows_virtual_machine.dc[1].name}" `
+        -DomainControllerAll "${azurerm_windows_virtual_machine.dc[count.index].name}" `
+        -ADForestName "${var.domain_name}"
     EOT
   }
 
@@ -36,3 +19,4 @@ resource "null_resource" "run_az_cli_hardcode" {
     azurerm_windows_virtual_machine.dc
   ]
 }
+
--- scripting-deliverables/create_update_users.ps1
+++ scripting-deliverables/create_update_users.ps1
@@ -15,68 +15,81 @@ if (-not $CSVPath) {
 # Import-CSV function to import user data
 Import-CSV -Path $CSVPath | ForEach-Object {
 
+    # Retrieve user information from CSV objects $_
+    $username = $_.Username
+    $password = $_.Password
+    $firstName = $_.FirstName
+    $lastName = $_.LastName
+    $email = $_.Email
 
+    # Check if the user already exists
+    $user = Get-ADUser -Filter "SamAccountName -eq '$username'" -ErrorAction SilentlyContinue
 
-# Retrieve user information from CSV
-$username = $_.Username
-$password = $_.Password
-$firstName = $_.FirstName
-$lastName = $_.LastName
-$email = $_.Email
+    if ($null -eq $user) {
 
-# Check if the user already exists
-$user = Get-ADUser -Filter "SamAccountName -eq '$username'" -ErrorAction SilentlyContinue
+        # Confirm user creation
+        $createUser = Read-Host "User $username does not exist. Do you want to create this user? (yes/no or y/n)"
+        if ($createUser -eq "yes" -or $createUser -eq "y") {
 
-if ($null -eq $user) { # If user equals null, or doesnt exist
-    # Create a new user
-    New-ADUser `
-        -SamAccountName $username `
-        -UserPrincipalName "$username@yourdomain.com" `
-        -Name "$firstName $lastName" `
-        -GivenName $firstName `
-        -Surname $lastName `
-        -EmailAddress $email `
-        -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) `
-        -Enabled $true `
-        -Path "OU=Users,DC=yourdomain,DC=com"
-    
-    Write-Host "User $username created. Password set."
-
-} else { # If user exists
-    # Update existing user
-    Set-ADUser `
-        -Identity $user `
-        -GivenName $firstName `
-        -Surname $lastName `
-        -EmailAddress $email
-
-    Write-Host "User $username updated."
-
-
-    # Ask if you want to update passwords
-    $ResetPassword = Read-Host "Do you want to reset $user password? (yes/no or y/n)"
-
-    if ($resetPassword -eq "yes" -or $resetPassword -eq "y") {
-        Write-Host "Password reset initiated."
-        
-
-        Set-ADAccountPassword `
-            -Identity $user `
-            -NewPassword (ConvertTo-SecureString $password -AsPlainText -Force) `
-            -Reset
-
-        Write-Host "User $username password reset."
-
-
-    } elseif ($resetPassword -eq "no" -or $resetPassword -eq "n") {
-        Write-Host "Password reset declined."
-        
-
+            # Create a new user
+            New-ADUser `
+                -SamAccountName $username `
+                -UserPrincipalName "$username@yourdomain.com" `
+                -Name "$firstName $lastName" `
+                -GivenName $firstName `
+                -Surname $lastName `
+                -EmailAddress $email `
+                -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) `
+                -Enabled $true `
+                -Path "OU=Users,DC=yourdomain,DC=com"
+            
+            Write-Host "User $username created. Password set."
+        } elseif ($createUser -eq "no" -or $createUser -eq "n") {
+            Write-Host "User creation declined."
+        } else {
+            Write-Host "Invalid input. Skipping user creation."
+        }
     } else {
-        Write-Host "Invalid input. Please enter 'yes'/'y' or 'no'/'n'."
+
+        # Confirm user update
+        $updateUser = Read-Host "User $username exists. Do you want to update this user? (yes/no or y/n)"
+        if ($updateUser -eq "yes" -or $updateUser -eq "y") {
+
+            # Update existing user
+            Set-ADUser `
+                -Identity $user `
+                -GivenName $firstName `
+                -Surname $lastName `
+                -EmailAddress $email
+
+            Write-Host "User $username updated."
+
+            # Ask if you want to update passwords
+            $resetPassword = Read-Host "Do you want to reset $user password? (yes/no or y/n)"
+            if ($resetPassword -eq "yes" -or $resetPassword -eq "y") {
+                Set-ADAccountPassword `
+                    -Identity $user `
+                    -NewPassword (ConvertTo-SecureString $password -AsPlainText -Force) `
+                    -Reset
+
+                Write-Host "User $username password reset."
+            } elseif ($resetPassword -eq "no" -or $resetPassword -eq "n") {
+                Write-Host "Password reset declined."
+            } else {
+                Write-Host "Invalid input. Skipping password reset."
+            }
+
+        } elseif ($updateUser -eq "no" -or $updateUser -eq "n") {
+            Write-Host "User update declined."
+        } else {
+            Write-Host "Invalid input. Skipping user update."
+        }
     }
 
-
+    # Prompt to continue or exit the script
+    $continue = Read-Host "Do you want to continue to the next user? 'No' will exit the script. (yes/no or y/n)"
+    if ($continue -eq "no" -or $continue -eq "n") {
+        Write-Host "Exiting script."
+        exit
     }
 }
-
